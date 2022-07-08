b8d762d247ab20c012110c95352d6f107596b698
sammykae <sammykay301@live.com>
2022-07-08 13:40:56 +0100

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,103 +1,161 @@
-import React, { Component } from 'react'
-import Logo from '../resources/images/logo.png'
+import React, { useEffect, useState } from "react";
+import Logo from "../resources/images/logo.png";
 
-import ViewHeadlineIcon from '@material-ui/icons/ViewHeadline'
+import ViewHeadlineIcon from "@material-ui/icons/ViewHeadline";
 
-import {NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
+const Header = () => {
+	const [navCollapsed, setNavCollapsed] = useState(false);
+	const onToggleNav = () => {
+		setNavCollapsed(!navCollapsed);
+	};
+	return (
+		<div>
+			<button
+				aria-expanded="false"
+				className="navbar-toggle collapsed"
+				onClick={onToggleNav}
+				type="button"
+				style={{ position: "fixed", zIndex: 1000 }}
+			>
+				<ViewHeadlineIcon style={{ color: "#a5c422" }} />
+			</button>
 
+			{navCollapsed && (
+				<div
+					className="navbar-collapse"
+					style={{
+						position: "fixed",
+						top: "0px",
+						left: 0,
+						right: 0,
+						zIndex: 99,
+					}}
+				>
+					<ul className="navi">
+						<div style={{ flexGrow: 1 }} className="heading">
+							<div>
+								<img src={Logo} alt="ICN" width="50" height="50" />
 
- class Header extends Component {
-     constructor(props) {
-         super(props)
-     
-         this.state = {
-            navCollapsed: false
-         }
-     }
-     onToggleNav = () => {
-        this.setState({ navCollapsed: !this.state.navCollapsed })
-      }
-    
-     
-    render() {
-        return (
-            <div >  
-            
-            <button
-            aria-expanded='false'
-            className='navbar-toggle collapsed'
-            onClick={this.onToggleNav}
-            type='button'
-            >
-           <ViewHeadlineIcon style={{ color: '#a5c422' }}/>
-          </button>
-          <div
-          className={(this.state.navCollapsed ? 'collapse' : '') + ' navbar-collapse'}
-          >
-               <ul
-           
-            className='navi'
-           
-            >
-                
-               <div style={{flexGrow:1}} className="heading">
-                        <div >
-                        <img src={Logo} alt="ICN" width="50" height="50"/>
-                        
-                   <span><i className="fa">SAGE HEALTH CENTER</i></span>
-                        </div>
-                    </div>
-    
-                    <li >
-                    <NavLink to ="/">
-                    <div href='/' className='nav-button' color='inherit'>Home</div>
-                    </NavLink>
-</li>
-<li>
-                    <NavLink to ="/about">
-                   
-                    <div   className='nav-button' color='inherit'>About</div>
-                    </NavLink>
-</li>
-<li>
-                    <NavLink to ="/doctor">
-                    
-                    <div  className='nav-button' color='inherit'>Doctors</div>
-                    </NavLink>
-</li>
-<li>
-                    <NavLink to ="/service"> 
-                    
-                    <div  className='nav-button' color='inherit'>Services</div>
-                    </NavLink>
-                    </li>
-                    <li>
-                    <NavLink to ="/appoint"> 
-                    
-                    <div  className='nav-button' color='inherit'>Appointment</div>
-                    </NavLink>
-                    </li>
-                    <li>
-                    <NavLink to ="/doctor_login"> 
-                    
-                    <div  className='nav-button' color='inherit'>Login</div>
-                    </NavLink>
-                   
-                </li>
+								<span>
+									<i className="fa">SAGE HEALTH CENTER</i>
+								</span>
+							</div>
+						</div>
 
-                    </ul>
+						<li>
+							<NavLink to="/">
+								<div href="/" className="nav-button" color="inherit">
+									Home
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/about">
+								<div className="nav-button" color="inherit">
+									About
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/doctor">
+								<div className="nav-button" color="inherit">
+									Doctors
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/service">
+								<div className="nav-button" color="inherit">
+									Services
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/appoint">
+								<div className="nav-button" color="inherit">
+									Appointment
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/doctor_login">
+								<div className="nav-button" color="inherit">
+									Login
+								</div>
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			)}
+			<div
+				className="desk"
+				style={{
+					position: "fixed",
+					top: "0px",
+					left: 0,
+					right: 0,
+					zIndex: 99,
+				}}
+			>
+				<ul className="navi">
+					<div style={{ flexGrow: 1 }} className="heading">
+						<div>
+							<img src={Logo} alt="ICN" width="50" height="50" />
 
-          </div>
-                 
-            
-              
-               
-            
-            
-            </div>
-        )
-    }
-}
+							<span>
+								<i className="fa">SAGE HEALTH CENTER</i>
+							</span>
+						</div>
+					</div>
 
-export default Header
+					<li>
+						<NavLink to="/">
+							<div href="/" className="nav-button" color="inherit">
+								Home
+							</div>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/about">
+							<div className="nav-button" color="inherit">
+								About
+							</div>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/doctor">
+							<div className="nav-button" color="inherit">
+								Doctors
+							</div>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/service">
+							<div className="nav-button" color="inherit">
+								Services
+							</div>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/appoint">
+							<div className="nav-button" color="inherit">
+								Appointment
+							</div>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/doctor_login">
+							<div className="nav-button" color="inherit">
+								Login
+							</div>
+						</NavLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
--- src/components/ViewAppoint.js
+++ src/components/ViewAppoint.js
@@ -1,80 +1,73 @@
+import React, { Component } from "react";
+import fire from "../config/fire";
+import Table from "./Table";
 
-import React, { Component } from 'react'
-import fire from '../config/fire'
-import Table from './Table'
+class ViewAppoint extends Component {
+	constructor(props) {
+		super(props);
 
+		this.state = {
+			contactObject: [],
+			dept: "",
+		};
+	}
 
+	componentDidMount() {
+		fire
+			.database()
+			.ref("store")
+			.on("value", (snapshot) => {
+				const user = this.props.user.email;
 
- class ViewAppoint extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            contactObject:[],
-             dept:''
-        }
-    }
-    
-    componentDidMount(){
-    
+				let dept = "";
+				if (snapshot.val() != null) {
+					const val = snapshot.val();
 
-fire.database().ref('store').on('value',(snapshot)=> {
-  const user=this.props.user.email
-  
-  let dept=''
-    if(snapshot.val()!=null){
-         const val=snapshot.val()
-         
-         Object.keys(val).map((id)=>{
-            if(val[id].email===user){
-                dept= val[id].department
-            }
-        })
-        
-         
-    }
-     fire.database().ref('appointment').on('value',(snapshot)=> {
-  
-  if(snapshot.val()!=null){
-       const vall=snapshot.val()
-       const data=Object.keys(vall).map(id=>{ 
-        if(vall[id].dept===dept){
-            return {...vall[id]}
-        }
-        else{
-            return null
-        }
-      }
-      
-      )
-      
-      const dat= Object.keys(data).filter(id=>
-        data[id]!==null
-      ).map(id=>({...data[id]}))
+					Object.keys(val).map((id) => {
+						if (val[id].email === user) {
+							dept = val[id].department;
+						}
+					});
+				}
+				fire
+					.database()
+					.ref("appointment")
+					.on("value", (snapshot) => {
+						if (snapshot.val() != null) {
+							const vall = snapshot.val();
+							const data = Object.keys(vall).map((id) => {
+								if (vall[id].dept === dept) {
+									return { ...vall[id] };
+								} else {
+									return null;
+								}
+							});
 
-     
-   this.setState({contactObject:dat, dept})
-  }
- 
-})
-   
-  })
+							const dat = Object.keys(data)
+								.filter((id) => data[id] !== null)
+								.map((id) => ({ ...data[id] }));
 
-    }
-      handleLogout=()=>{
-        fire.auth().signOut()
-    }
+							this.setState({ contactObject: dat, dept });
+						}
+					});
+			});
+	}
+	handleLogout = () => {
+		fire.auth().signOut();
+	};
 
-    render() {
-        return (
-            <div className='view-appoint'>
-               
-                <button className='btn btn-danger logout' onClick={this.handleLogout}>LOGOUT</button><br/>
+	render() {
+		return (
+			<div className="view-appoint">
+				<button className="btn btn-danger logout" onClick={this.handleLogout}>
+					LOGOUT
+				</button>
+				<br />
 
-               <Table dept={this.state.dept} data={this.state.contactObject}/>
-            </div>
-        )
-    }
+				<Table dept={this.state.dept} data={this.state.contactObject} />
+			</div>
+		);
+	}
 }
 
-export default ViewAppoint
+export default ViewAppoint;
